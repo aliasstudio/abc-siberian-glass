fb7f9631fc6fc352157665dd3d400dc3cd738616
aliasstudio <ownerofyourcode@gmail.com>
2025-01-19 11:24:38 +0500

    build: обновление зависимостей

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from '@app/app.config';
 import { AppComponent } from '@app/app.component';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 bootstrapApplication(AppComponent, appConfig)
   // eslint-disable-next-line no-console
